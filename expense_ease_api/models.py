ab610f4b6dd7f28bbae175fbda8d16b5e7b7d046
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2024-05-02 20:40:31 -0300

    create database models

--- expense_ease_api/models.py
+++ expense_ease_api/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+from .managers import UserManager
+
+Transaction_Types = models.TextChoices("incoming", "expense")
+
+
+class Base(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+
+    objects = UserManager
+
+    def __str__(self):
+        return self.email
+
+
+class Transaction(Base):
+    transaction_type = models.CharField(
+        max_length=100, choices=Transaction_Types.choices
+    )
+    value = models.DecimalField(decimal_places=2, max_digits=20)
+    category = models.CharField(max_length=120, blank=True, default="")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    payment_method = models.CharField(max_length=80, blank=True, default="")
