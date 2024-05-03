fadc07ea4ec7da049d9c629020ac6fb6df54d8c0
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2024-05-03 13:41:11 -0300

    create models serializers

---
+++ expense_ease_api/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from .models import User, Transaction
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "email", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    # Create user with hashed password
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = [
+            "id",
+            "transaction_type",
+            "value",
+            "category",
+            "user",
+            "date",
+            "payment_method",
+        ]
