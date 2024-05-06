c392d43c3ce46d9062eeb1b346e26b5a99280d19
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2024-05-06 20:59:41 -0300

    create SignUp endpoint

---
+++ expense_ease_api/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+
+from .views import SignUpAPIView
+
+urlpatterns = [path("signup/", SignUpAPIView.as_view())]
--- expense_ease_api/views.py
+++ expense_ease_api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class SignUpAPIView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid()
+        user = serializer.save()
+
+        refresh = RefreshToken.for_user(user)
+
+        return Response(
+            {
+                "access": str(refresh.access_token),
+                "refresh": str(refresh),
+                "user": serializer.data,
+            },
+            status=status.HTTP_201_CREATED,
+        )
